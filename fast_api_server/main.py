a85bfa4af50423fc08e130dff3a3a3eac67af118
Perehinets Maksym <supercomf128@gmail.com>
2024-01-17 18:21:05 +0200

    database file refactor

--- fast_api_server/main.py
+++ fast_api_server/main.py
@@ -15,7 +15,8 @@ models.Base.metadata.create_all(bind=engine)
 
 origins = [
     "http://localhost"
-    "http://localhost:8080",
+    "http://localhost:5500",
+	"http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
