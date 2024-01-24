216f65b2b8dcbea37c81c77d1953fae848e3c392
YuranJo <y14r06a062006@gmail.com>
2024-01-24 15:26:49 +0200

    mewmew

--- front_end/javascript/PostAndGetDataToBackEnd.js
+++ front_end/javascript/PostAndGetDataToBackEnd.js
@@ -16,22 +16,27 @@ function SendData(dataToSend, url) {
     });
 }
 
-function getData(url) {
+
+async function getData(url) {
   const backendUrl = "http://127.0.0.1:8000/test";
   console.log("sent");
-  var jsonData;
-  async function fetchData() {
-    try {
-      var response = await axios.get(backendUrl);
-      return response.data;
-    } catch (error) {
-      console.error("Помилка при отриманні даних:", error.message);
-    }
+
+  try {
+    const response = await axios.get(backendUrl);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Помилка при отриманні даних:", error.message);
+    throw error;
   }
-  console.log(jsonData);
-  jsonData = fetchData();
-  console.log(jsonData);
-  return jsonData;
 }
 
-console.log(getData("http://127.0.0.1:8000/test"));
+// Example usage
+(async () => {
+  try {
+    const jsonData = await getData("http://127.0.0.1:8000/test");
+    console.log("Final result", jsonData);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+})();
