71e920a55ee026ac4226ea8e6f455483ddf50abd
Perehinets Maksym <supercomf128@gmail.com>
2024-01-25 18:15:17 +0200

    stable test version if pasrser.py

---
+++ fast_api_server/service.py
@@ -0,0 +1,38 @@
+
+class Service:
+    def __init__(self):
+        pass
+
+    def format_spell(self, data):
+
+        formatted_result = [
+            {
+                "id": spell.id,
+                "name": spell.spell_name,
+                "source": source,
+                "page": spell.book_page,
+                "level": spell.spell_level,
+                "school": spell.school,
+                "components": spell.components,
+                # Duration type, time, concentration
+                "duration": {
+                    "type": duration.duration_type,
+                    "time": duration.duration_time,
+                    "concentration": duration.concentration
+                },
+                "time": spell.cast_time,
+                # Range with
+                "ranges": {
+                    "type": ranges.shape,
+                    "distance": {
+                        "type": ranges.distance_type,
+                        "amount": ranges.distance_range
+                    }
+                },
+                "casters": spell.suitable_casters,
+                "description": spell.spell_description,
+                "descriptionOnHigherLevels": spell.entries_higher_level
+            }
+            for spell, source, duration, ranges in data
+        ]
+        return formatted_result
