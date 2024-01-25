42b8ffe5092aadf02d171810847ab72f41678b80
YuranJo <y14r06a062006@gmail.com>
2024-01-25 15:53:45 +0200

    commit pizda hui chainik ne rabotae class suka [0,2,3,4,5,6,7,8,9,10] debyrbvyirueveureuub

--- front_end/javascript/showSpellinfo.js
+++ front_end/javascript/showSpellinfo.js
@@ -5,7 +5,10 @@ var spellName,
   spellComponents,
   spellDescriptio,
   spellLevel,
-  spellBookName;
+  spellBookName,
+  spellBookPage,
+  spellOnHigherLvl,
+  spellCasters;
 
 document.addEventListener("DOMContentLoaded", function () {
   var spellListTable = document.getElementById("spellList");
@@ -39,6 +42,9 @@ function insertInfoIntoDescription(data) {
   spellDescription = document.getElementById("spellDescription");
   spellLevel = document.getElementById("spellLevel");
   spellBookName = document.getElementById("bookName");
+  spellBookPage = document.getElementById("bookPage");
+  spellCasters = document.getElementById("casters");
+  spellOnHigherLvl = document.getElementById("descOnHigherLvl");
 
   spellName.innerHTML = sdata.name;
 
@@ -63,6 +69,8 @@ console.log(sdata.ranges);
 
   spellBookName.innerHTML = sdata.source;
 
+  spellBookPage.innerHTML = `Page: ${sdata.page}`;
+
   spellComponents.innerHTML = " "
   if (sdata.components.v) {
     spellComponents.innerHTML = "V";
@@ -77,7 +85,14 @@ console.log(sdata.ranges);
       ? ` M - ${sdata.components.m.text}`
       : ` M - ${sdata.components.m}`;
   }
+
+  spellOnHigherLvl.innerHTML = sdata.descriptionOnHigherLevels[0].entries;
+
+
+  spellCasters.innerHTML = `Casters: ${sdata.casters[0].name}`;
 }
+
+
 
 // async function loadData(){
 //   try {
