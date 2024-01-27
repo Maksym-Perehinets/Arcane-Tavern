9a963b6a5fa9c7a25558c16065559a255e1e63ff
IhorIvantco <91619730+IhorIvantco@users.noreply.github.com>
2024-01-27 16:12:27 +0200

    ідіть нахуй всі

--- front_end/javascript/SendFilterData.js
+++ front_end/javascript/SendFilterData.js
@@ -3,10 +3,12 @@ let filterList = [];
 async function sendFilters(filters) {
   let level, caster_class, school, damage_type, range_distance, range_type, range_shape, duration_time, duration_type, casting_time, casting_type = "";
 
+
   level = filters[0];
   caster_class = filters[1];
   school = filters[2];
-  damage_type = filters[3];
+  try{
+  damage_type = '';}catch(error){console.log("ПОХУЙ БЛЯТЬ");}
 
   // range_distance = filters[4].split(" ")[0];
   // range_type = filters[4].split(" ")[1];
@@ -22,7 +24,7 @@ async function sendFilters(filters) {
       break;
     default:
       range_type = filters[4].split(" ")[0]; 
-      range_distance = filters[4].split(" ")[1].value;
+      range_distance = filters[4].split(" ")[1].split("-")[0];
     }
 
   switch(filters[5].split(" ").length){
@@ -41,7 +43,7 @@ async function sendFilters(filters) {
     casting_type = filters[6].split(" ")[1];
   
 
-  console.log(range_distance);
+  console.log(level, caster_class, school, damage_type, range_distance, range_type, range_shape, duration_time, duration_type, casting_time, casting_type);
 
   res = await SortFilters(level, caster_class, school, damage_type, range_distance, range_type, range_shape, duration_time, duration_type, casting_time, casting_type);
   ShowFiltered(res);
@@ -58,9 +60,12 @@ async function sendFilters(filters) {
 function collectFilters(){
     var elms = document.querySelectorAll("[id='actionDropdown']");
     for(var i = 0; i < elms.length; i++){ 
+      try{
       filterList.push(elms[i].options[elms[i].selectedIndex].text.replace(/\(|\)/g, ""));
+    } catch(error){
+      console.log("Похуй на " + error);
     }
-    console.log(filterList);
+    }
     sendFilters(filterList);
     filterList = [];
 }
