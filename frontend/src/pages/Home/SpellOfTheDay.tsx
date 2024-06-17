46398b278256955a2f36a9a168ea5bc0eab91aa4
MrVoiceRAR <lealoria117@gmail.com>
2024-06-17 13:36:15 -0400

    Transition-SpellOfTheDay

--- frontend/src/pages/Home/SpellOfTheDay.tsx
+++ frontend/src/pages/Home/SpellOfTheDay.tsx
@@ -1,31 +1,44 @@
-import {useState} from 'react';
 
+import { useState, useEffect } from 'react';
 import SpellOfTheDayDiv from "./SpellOfTheDayDiv";
 
 const SpellOfTheDay = () => {
-
   const [btnStatus, setBtnStatus] = useState(0);
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    if (fade) {
+      const timeout = setTimeout(() => {
+        setFade(false);
+      }, 500); // Duration of the fade effect in ms
+      return () => clearTimeout(timeout);
+    }
+  }, [fade]);
+
+  const handleClick = (status) => {
+    setFade(true);
+    setTimeout(() => setBtnStatus(status), 500); // Match the duration of the fade effect
+  };
 
   const showSpell = () => {
-    return ( btnStatus
+    return btnStatus
       ? (<SpellOfTheDayDiv />) 
-      : (<img className="spellCard" src="https://dvoxsotka.s3.amazonaws.com/arcane-tavern/spellCard.png" alt="Alchemy in action" />)
-    );
+      : (<img className="spellCard" src="https://dvoxsotka.s3.amazonaws.com/arcane-tavern/spellCard.png" alt="Alchemy in action" />);
   };
 
   return (
     <div className="daily-spell-div">
-            <div className="show-spell-div">
-            {showSpell()}
-            </div>
-            
-            <p className="title-text">Do you want to reveal your spell of the day?</p>
-            <div className="btn-div">
-                <button onClick={() => setBtnStatus(0)} className="no-btn">No</button>
-                <button onClick={() => setBtnStatus(1)} className="yes-btn">Yes</button>
-            </div>
+      <div className={`show-spell-div ${fade ? 'fade' : ''}`}>
+        {showSpell()}
+      </div>
+      
+      <p className="title-text">Do you want to reveal your spell of the day?</p>
+      <div className="btn-div">
+        <button onClick={() => handleClick(0)} className="no-btn">No</button>
+        <button onClick={() => handleClick(1)} className="yes-btn">Yes</button>
+      </div>
     </div>
   )
 }
 
-export default SpellOfTheDay
+export default SpellOfTheDay;
