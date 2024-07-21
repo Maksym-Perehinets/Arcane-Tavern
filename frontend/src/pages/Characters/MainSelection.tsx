7bd399586b58640e398319e123ff973ee49d6ed6
YuranJo <y14r06a062006@gmail.com>
2024-07-22 00:51:15 +0200

    characters-page summary left-side 1/2

--- frontend/src/pages/Characters/MainSelection.tsx
+++ frontend/src/pages/Characters/MainSelection.tsx
@@ -1,12 +1,14 @@
 const MainSelection = () => {
+
+  const navSelection = ["Summary", "Combat", "Probiciency", "Spells", "Features", "Equipment"]
+
   return (
     <>
-    <div className="selection-div mselection first">Summary</div>
-    <div className="selection-div mselection">Combat</div>
-    <div className="selection-div mselection">Proficiency</div>
-    <div className="selection-div mselection">Spells</div>
-    <div className="selection-div mselection">Features</div>
-    <div className="selection-div mselection">Equipment</div>
+    <div className="mt-20 ml-[10%] rounded p-0 ">
+      {navSelection.map((value: string, index: number) => {
+        return <div className="selection-div mselection" tabIndex={index}>{value}</div>
+      })}
+    </div>
     </>
     
   )
--- frontend/src/pages/Characters/MainSelectionOUt/Summary.tsx
+++ frontend/src/pages/Characters/MainSelectionOUt/Summary.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 
 const Summary = () => {
-  return (
-    <div className="summary-div">
-        <div className="selection-div stats-div">STR</div>
-        <div className="selection-div stats-div">DEX</div>
-        <div className="selection-div stats-div">CON</div>
-        <div className="selection-div stats-div">INT</div>
-        <div className="selection-div stats-div">WIS</div>
-        <div className="selection-div stats-div">CHA</div>
 
-        <div className="half-div">
+    const cardStats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
+    const demoStats = [10,9,2,5,7,8];
+    const demoMod = [1,2,0,1,1,2];
+    const savingThrows = [];
+
+
+  return (
+    <div className="summary-div ml-[0%]">
+        <div className="ml-[7%] mt-[2%] flex">
+            {cardStats.map((value: string,index: number) => {
+                return <div 
+                tabIndex={index}
+                className="selection-div stats-div ml-[3%] flex flex-col justify-between items-center">
+                    <p className="w-full border-b-2 border-indigo-400/70 pb-2">{value}</p>
+                    <p className="text-6xl font-bold text-gray-400">{demoStats[index]}</p>
+                    <div className="bg-indigo-400/70 rounded-full w-[50%] h-fit mb-1">+{demoMod[index]}</div>
+            </div>
+            })}
+        </div>
+         <div className="half-div">
             <div className="selection-div main-stats first">10</div>
             <div className="selection-div main-stats">9</div>
             <div className="selection-div main-stats">2</div>
@@ -18,7 +30,7 @@ const Summary = () => {
             <div className="selection-div main-stats">7</div>
             <div className="selection-div main-stats">8</div>
         </div>
-        <div className="half-div right">
+        {/* <div className="half-div right">
             <div className="selection-div saving-throws">Saving throws</div>
         </div>
 
@@ -27,7 +39,7 @@ const Summary = () => {
         </div>
         <div className="half-div right">
             <div className="selection-div other-stats notes">Notes</div>
-        </div>
+        </div> */}
 
     </div>
   )
