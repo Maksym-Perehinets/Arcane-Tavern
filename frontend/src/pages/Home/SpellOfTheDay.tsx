e55f32709e446247172b681f66dbc8ffabe73f61
ihor <igorivantco@gmail.com>
2024-07-25 02:42:22 +0300

    funny flip

--- frontend/src/pages/Home/SpellOfTheDay.tsx
+++ frontend/src/pages/Home/SpellOfTheDay.tsx
@@ -1,43 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 import SpellOfTheDayDiv from "./SpellOfTheDayCard";
 
 const SpellOfTheDay = () => {
-  const [btnStatus, setBtnStatus] = useState(0);
-  const [fade, setFade] = useState(false);
+  const [flipped, setFlipped] = useState(false);
 
-  useEffect(() => {
-    if (fade) {
-      const timeout = setTimeout(() => {
-        setFade(false);
-      }, 500);
-      return () => clearTimeout(timeout);
-    }
-  }, [fade]);
-
-  const handleClick = (status: number) => {
-    setFade(true);
-    setTimeout(() => setBtnStatus(status), 500);
-  };
-
-  const showSpell = () => {
-    return btnStatus
-      ? (<SpellOfTheDayDiv />) 
-      : (<img className="spellCard" src="https://dvoxsotka.s3.amazonaws.com/arcane-tavern/spellCard.png" alt="Alchemy in action" />);
+  const handleCardClick = () => {
+    setFlipped(!flipped);
   };
 
   return (
-    <div className="daily-spell-div flex-col">
-      <div className={`show-spell-div ${fade ? 'fade' : ''}`}>
-        {showSpell()}
-      </div>
-      
-      <p className="title-text">Reveal your spell of the day</p>
-      <div className="btn-div">
-        <button onClick={() => handleClick(0)} className="no-btn">No</button>
-        <button onClick={() => handleClick(1)} className="yes-btn">Yes</button>
+    <div className="daily-spell-div flex justify-center">
+      <div
+        className={`show-spell-div ${flipped? "transform-rotate-y-180" : ""} flex flex-col items-center transition-all duration-700 `}
+        onClick={() => handleCardClick()}
+      >
+        <SpellOfTheDayDiv  />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SpellOfTheDay;
