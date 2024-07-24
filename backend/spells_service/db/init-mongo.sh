60c5d3160203ac0388b38c15587ebcdc6fb2d7e3
Perehinets Maksym <supercomf128@gmail.com>
2024-07-25 00:26:05 +0300

    Encasement: updated entry point for populating mongodb

--- backend/spells_service/db/init-mongo.sh
+++ backend/spells_service/db/init-mongo.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+# Ensure DATABASE_NAME is set
+if [ -z "$DATABASE_NAME" ]; then
+  echo "DATABASE_NAME is not set. Exiting."
+  exit 1
+fi
+
+echo "DATABASE_NAME is set to: $DATABASE_NAME"
+
 for filename in /docker-entrypoint-initdb.d/*.json; do
   collection_name=$(basename "$filename" .json)
-  mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --file "$filename"
+  echo "Processing file: $filename"
+  echo "Importing into collection: $collection_name"
+
+  if [[ $filename != *"spell"* ]]; then
+    echo "Standard import for: $filename"
+    mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --file "$filename" --drop
+  else
+    echo "Special import for spell file: $filename"
+    jq -c '.spell[]' "$filename" | while read -r spell; do
+      echo "Importing spell: $spell"
+      echo "$spell" | mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --type json
+    done
+  fi
 done
