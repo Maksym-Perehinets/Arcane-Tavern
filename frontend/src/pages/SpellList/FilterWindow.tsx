e2b2ca062fd7abd52842a039f4bd636c17cff89b
YuranJo <y14r06a062006@gmail.com>
2024-07-24 23:26:28 +0200

    minor changes mysterious hover removed

--- frontend/src/pages/SpellList/FilterWindow.tsx
+++ frontend/src/pages/SpellList/FilterWindow.tsx
@@ -4,32 +4,21 @@ import { useState } from "react"
 
 
 const FilterWindow = () => {
-
-    const [isVisible, setVisibility] = useState(false)
-    const [translateOnX, setTranslateOnX] = useState(0);
-
-    const toggleVisibility = () => {
-        isVisible ? setTranslateOnX(prev => prev + 120) : setTranslateOnX(prev => prev - 120)
-        setVisibility(!isVisible)
-    }
-
-
+    
     return (
-        <div 
-            className="flex" 
-            style={{
-                transform: `translate(${translateOnX}px)`,
-                transition: 'transform 0.5s'}}>
-            <div className="rounded-r-xl bg-opacity-95 bg-funny-purple h-min w-min flex flex-col p-4 gap-4">
+        <div className="flex">
+            <div className="rounded-r-xl bg-opacity-75 bg-black/75 h-min w-min 
+            flex-col p-3 font-semibold text-gray-400 border-2 border-stone-400/25 border-l-0">
                 {filterWindow.map((elem: IFilterWindow) => (
-                    <p key={elem.id} className="flex gap-2 mr-8">
-                        <img 
+                    <div key={elem.id} className="cursor-pointer hover:text-violet-300/50 transition-all group flex-col">
+                        <img
+                            className="flex w-[50%] self-center ml-3 my-2"
                             key={elem.id} 
                             src={elem.icon} 
                             alt={elem.id} 
                         />
-                        {elem.label}
-                    </p>
+                        <p className="opacity-0 group-hover:opacity-100 text-center text-xl">{elem.label}</p>
+                    </div>
                 ))}
             </div>
         </div>
