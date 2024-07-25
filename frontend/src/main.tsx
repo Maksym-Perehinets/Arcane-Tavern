a4f4579aa64179c3e78e59f0101ce8c0c6c23829
ihor <igorivantco@gmail.com>
2024-07-26 00:00:35 +0300

    Queries setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import './index.css'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+      <App />
   </BrowserRouter>
 );
