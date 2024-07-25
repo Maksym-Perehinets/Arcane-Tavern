31725f9d3935b6ce8f99cf307439a28b52820da3
Perehinets Maksym <supercomf128@gmail.com>
2024-07-25 14:03:53 +0300

    Encasement: updated docker files and entry point for proper work of db and dev mod in go Dockerfile

--- backend/spells_service/db/init-mongo.sh
+++ backend/spells_service/db/init-mongo.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-# Ensure DATABASE_NAME is set
-if [ -z "$DATABASE_NAME" ]; then
-  echo "DATABASE_NAME is not set. Exiting."
-  exit 1
-fi
-
-echo "DATABASE_NAME is set to: $DATABASE_NAME"
-
 for filename in /docker-entrypoint-initdb.d/*.json; do
   collection_name=$(basename "$filename" .json)
-  echo "Processing file: $filename"
-  echo "Importing into collection: $collection_name"
 
   if [[ $filename != *"spell"* ]]; then
-    echo "Standard import for: $filename"
+    # Standard import for all files sources and index
     mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --file "$filename" --drop
   else
-    echo "Special import for spell file: $filename"
-    jq -c '.spell[]' "$filename" | while read -r spell; do
-      echo "Importing spell: $spell"
-      echo "$spell" | mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --type json
+    # For spell file import differs in order to gave each of them a unique _id
+    jq -c '.spell[]' "$filename" | while read -r spell; do # Remove the array and iterate over the objects
+      mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --type json
     done
   fi
 done
