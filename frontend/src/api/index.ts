c575d04a79e703c992d8a0c38a61bbdc76b8db23
ihor <igorivantco@gmail.com>
2024-07-26 15:46:47 +0300

    Setup

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const backendUrl = "http://127.0.0.1:8080";
+
+export async function getAllSpells(per_page: number, page: number) {
+  const apiUrl = `${backendUrl}/spells&per_page=${per_page}&page=${page}`;
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getSpellById(spellId: number) {
+  const apiUrl = `${backendUrl}/get-spell/${spellId}`;
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function sortTableData(filterName: string, sortingType: boolean) {
+  const apiUrl = `${backendUrl}/data-sort/?filter_name=${filterName}&asc_value=${sortingType}`;
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function applyFilters(filters: string) {
+  const apiUrl = `${backendUrl}/data-filter/?${filters.substring(1)}`;
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
