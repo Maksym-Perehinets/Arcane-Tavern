43ae41f3fc5be88f86875f736ca38fc08e6b8993
Perehinets Maksym <supercomf128@gmail.com>
2024-07-26 13:00:48 +0300

    Enhancement: changed init script for bootstrapping data into mongodb

--- backend/spells_service/db/init-mongo.sh
+++ backend/spells_service/db/init-mongo.sh
@@ -4,12 +4,14 @@ for filename in /docker-entrypoint-initdb.d/*.json; do
   collection_name=$(basename "$filename" .json)
 
   if [[ $filename != *"spell"* ]]; then
-    # Standard import for all files sources and index
-    mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --file "$filename" --drop
+    # add each document to the collection as separate element
+    jq -c 'to_entries | map({ (.key): .value })' "$filename" | while read -r doc; do
+      mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --type json --jsonArray --file <(echo "$doc")
+    done
   else
     # For spell file import differs in order to gave each of them a unique _id
     jq -c '.spell' "$filename" | while read -r spell; do # Remove the array and iterate over the objects
-      mongoimport --db "$DATABASE_NAME" --collection "$collection_name" --type json --jsonArray --file <(echo "$spell") --drop
+      mongoimport --db "$DATABASE_NAME" --collection "spells" --type json --jsonArray --file <(echo "$spell")
     done
   fi
 done
